2e41b5dafe9f306e3c5e1dc2e9785e38e9ef0bad
Cameron <110361869+20220856@users.noreply.github.com>
2022-08-08 14:57:26 +1200

    Create First-Code-Calculator-08.08.py

---
+++ Software-Development/Code/Python/First-Code-Calculator-08.08.py
@@ -0,0 +1,16 @@
+Num1 = int(input("Enter First Number: "))
+Num2 = int(input("Enter Second Number: "))
+Opp = input("Enter an Arithmetic Operation: (+, -, *, /) ")
+if (Opp == "+"):
+    sum=Num1+Num2
+elif (Opp == "-"):
+    sum=Num1-Num2
+elif (Opp == "*"):
+    sum=Num1*Num2
+elif (Opp == "/"):
+    sum=Num1/Num2
+print("%d %s %d = %d" %(Num1, Opp, Num2, sum))
+
+#sum = Num1 + Num2
+#print(sum)
+#print("%d+%d=%d" %(Num1, Num2, sum)) # Can insert integers whilst keeping a string using %d
