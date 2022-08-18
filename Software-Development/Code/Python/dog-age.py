60b58e3c4e2ed936865b69cdb00bfc90b8859c0d
Cameron <110361869+20220856@users.noreply.github.com>
2022-08-18 13:02:34 +1200

    Update dog-age.py

--- Software-Development/Code/Python/dog-age.py
+++ Software-Development/Code/Python/dog-age.py
@@ -1,9 +1,11 @@
 # Write a program that calculates a dog's age in dog years.
 # The first two human years are 10.5 dog years, and any after that are 4 human years.
-
-humanage=float(input("How old is the dog in Human years? "))
-if humanage<=2:
-    dogage=humanage*10.5
-else:
-    dogage=(21+((humanage-2)*4))
-print("The dog is",dogage,"dog years old.")
+try:
+    humanage=float(input("How old is the dog in Human years? "))
+    if humanage<=2:
+        dogage=humanage*10.5
+    else:
+        dogage=(21+((humanage-2)*4))
+    print("The dog is",dogage,"dog years old.")
+   except:
+    print("Please insert a number")
