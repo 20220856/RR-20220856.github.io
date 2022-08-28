a396647b04c559202345990d5a4aa142d24ef113
Cameron <110361869+20220856@users.noreply.github.com>
2022-08-29 10:41:10 +1200

    Update First-Code-Calculator-08.08.py

--- Software-Development/Code/Python/First-Code-Calculator-08.08.py
+++ Software-Development/Code/Python/First-Code-Calculator-08.08.py
@@ -1,18 +1,14 @@
-#This is my first attempt at writing code.
+# This is my first attempt at writing code.
 
-Num1 = int(input("Enter First Number: "))
-Num2 = int(input("Enter Second Number: "))
-Opp = input("Enter an Arithmetic Operation: (+, -, *, /) ")
+Num1 = int(input("Enter First Number: ")) # This is the assignment of the variable for the first number
+Num2 = int(input("Enter Second Number: ")) # This is the assignment of the variable for the second number
+Opp = input("Enter an Arithmetic Operation: (+, -, *, /) ") # This is the assignment of a third variable, responsible for an arithmetic operation.
 if (Opp == "+"):
-    sum=Num1+Num2
+    sum=Num1+Num2 # This operation adds the first and second number if the arithmetic input is "+"
 elif (Opp == "-"):
-    sum=Num1-Num2
+    sum=Num1-Num2 # This operation subtracts the second number from the first if the arithmetic input is "-"
 elif (Opp == "*"):
-    sum=Num1*Num2
+    sum=Num1*Num2 # This operation multiplies the first and second number if the arithmetic input is "*"
 elif (Opp == "/"):
-    sum=Num1/Num2
-print("%d %s %d = %d" %(Num1, Opp, Num2, sum))
-
-#sum = Num1 + Num2
-#print(sum)
-#print("%d+%d=%d" %(Num1, Num2, sum)) # Can insert integers whilst keeping a string using %d
+    sum=Num1/Num2 # This operation divides the first number by the second number if the arithmetic input is "/"
+print("%d %s %d = %d" %(Num1, Opp, Num2, sum)) # This operation provides readable formatting; it takes user input, and provides an output dependant on user inputs. In practice, it prints the first number, the arithmetic operation, the second number, and the result of the arithmetic operation (eg. 2 + 4 = 6 , 3 * 5 = 15 , etc.).
