36f278ab41cab5eae8fb990205a3d17513c3909f
Cameron <110361869+20220856@users.noreply.github.com>
2022-08-29 11:36:55 +1200

    Update weird-math.py

--- Software-Development/Code/Python/weird-math.py
+++ Software-Development/Code/Python/weird-math.py
@@ -3,13 +3,13 @@
 #nums=list(map(int, input("enter multiple values").split()))
 #print(nums)
 
-try:
-    x,y=[int(x) for x in input("Enter two whole numbers: ").split()]
-    if x and y%(2 or 3)==0:
-        z=(x*y)
+try: # This TRY assumes TWO integer user inputs, and redirects to the EXCEPT if it is anything else
+    x,y=[int(x) for x in input("Enter two whole numbers: ").split()] # It took me such a long time to wrap my head around the use of "int(x) for x in input", but I now know this it is used to ensure that the temporary value "x" is assigned type INT (therefore meaning both "x" and "y" are of type INT). The split() is to ensure that the resulting inputs ("x" and "y") are assigned to their respective variables, rather than "x" being assigned both inputs and "y" being assigned nothing.
+    if x and y%(2 or 3)==0: # Checks that BOTH inputs are either divisible by two OR three with a remainder of zero (ie. directly divisible)
+        z=(x*y) # Multiplies the inputs and prints the result
         print("Both numbers are divisible by either 2 or 3, and their multiplied value is:",z)
-    else:
+    else: # If one or both inputs are not divisible by two OR three, adds them together instead, and prints the result
         z=(x+y)
         print("One or both numbers are not divisible by neither 2 nor 3, but their sum is",z)
-except ValueError:
+except ValueError: # If the input is NOT specifically TWO integers, prints this error
     print("Error: Please ensure you enter two whole numbers.")
