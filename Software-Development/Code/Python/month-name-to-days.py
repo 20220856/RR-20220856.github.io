b0b20f0253468e28d7ba13553b05500e62343055
Cameron <110361869+20220856@users.noreply.github.com>
2022-08-29 11:11:29 +1200

    Update month-name-to-days.py

--- Software-Development/Code/Python/month-name-to-days.py
+++ Software-Development/Code/Python/month-name-to-days.py
@@ -1,14 +1,15 @@
 # Write a program to convert month name to the number of days correspondant to that month
 
-month=str(input("Input the Month name: "))
-feb="February has 28 or 29 days."
-a=["January","March","May","July","August","October","December"]
-b=["April","June","September","November"]
+month=str(input("Input the Month name: ")) # Requests a string input from the user
+feb="February has 28 or 29 days." # February is its own variable, as it is the only month with an entirely new, non-fixed value
+a=["January","March","May","July","August","October","December"] # Months with 31 days are grouped into a LIST
+b=["April","June","September","November"] # Months with 30 days are grouped into a second LIST
 if month==("February"):
-    print(feb)
-for i in a:
-    if i==month:
+    print(feb) # If the month happens to be February, prints February's own message
+for i in a: # My first use of a FOR loop, uses "i" as a temporary value to scan through the contents of list [a]
+    if i==month: # When the temporary value "i" is the same as the user input as outlined in list [a], prints the 31 day message
         print(month,"has 31 days.")
 for i in b:
-    if i==month:
+    if i==month: # When the temporary value "i" is the same as the user input as outlined in list [b], prints the 30 day message
         print(month,"has 30 days.")
+# I found this to be a very troublesome piece of code when I was not aware of for loops, and very easy after having figured them out.
