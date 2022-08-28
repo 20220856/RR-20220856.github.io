5f8c5505f3eaf90550bd1bdd304f644bc2dbd174
Cameron <110361869+20220856@users.noreply.github.com>
2022-08-29 11:14:52 +1200

    Update string-check.py

--- Software-Development/Code/Python/string-check.py
+++ Software-Development/Code/Python/string-check.py
@@ -1,6 +1,7 @@
 # This program determines whether an input is an integer or not.
-try:
-    a=int(input("Input a string: "))
-    print("The input is an integer.")
-except ValueError:
+try: # This TRY sends the user to the EXCEPT in the event that the input is not an integer.
+    a=int(input("Input a string: ")) # This code runs on the assumption that the input IS an integer, as it would not be able to run if it were not.
+    print("The input is an integer.") # I found this to be an interesting way to do this - I dont know if it was the "intended" way, but I am quite the fan.
+except ValueError: # If the TRY is not positively fulfilled, prints the error message.
     print("The input is not an integer.")
+# I quite like this one.
